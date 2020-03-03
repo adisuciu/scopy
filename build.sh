6b78dc7d5f41ee5cadd231ecc8d352b63bc2c739
Adrian Suciu <adrian.suciu1988@gmail.com>
2020-03-03 21:17:21 +0200

    update

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -e
 SCOPY_MINGW_BUILD_DEPS_FORK=adisuciu
 SCOPY_MINGW_BUILD_DEPS_BRANCH=master
 
-export PATH=$PATH:/bin:/usr/bin:/${MINGW_VERSION}/bin:/c/Program\ Files/Git/cmd:/c/Windows/System32:/c/Program\ Files/7-Zip
+export PATH=/bin:/usr/bin:/${MINGW_VERSION}/bin:/c/Program\ Files/Git/cmd:/c/Windows/System32:/c/Program\ Files/7-Zip:/c/Program\ Files\ (x86)/Inno\ Setup \5
 echo $PATH
+
 WORKDIR=${PWD}
 echo BUILD_NO $BUILD_NO
 JOBS=-j3
